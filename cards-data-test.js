75657c6caa3d7b47d7705af318ca1370e6d41557
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-11-18 12:50:45 -0700

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -1,6 +1,5 @@
 export default [
 { href: './html/Buckshot Roulette/index.html', imgSrc: 'buckshot roulette.jpeg', name: 'Buckshot Roulette' , 'page': 5, 'id': 763 },
-{ href: './html/Survival Race/index.html', imgSrc: 'survival race.jpeg', name: 'Survival Race' , 'page': 5, 'id': 982 },
 { href: './html/Supermarioplay/index.html', imgSrc: 'supermarioplay.jpeg', name: 'Supermarioplay' , 'page': 5, 'id': 981 },
 { href: './html/Sticky Sorcerer/index.html', imgSrc: 'sticky sorcerer.png', name: 'Sticky Sorcerer' , 'page': 5, 'id': 979 },
 { href: './html/Stickman Clash/index.html', imgSrc: 'stickman clash.jpeg', name: 'Stickman Clash' , 'page': 5, 'id': 978 },

322a3a1c69b11fc99152256e7a29527a7bf77f44
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-11-18 12:51:44 -0700

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/Burnin rubber/index.html', imgSrc: 'Burnin rubber.jpeg', name: 'Burnin rubber' , 'page': 5, 'id': 772 },
   { href: './html/Capybara clicker/index.html', imgSrc: 'capybara clicker.jpeg', name: 'Capybara clicker' , 'page': 5, 'id': 777 },
   { href: './html/Case Clicker 2/index.html', imgSrc: 'case clicker 2.jpeg', name: 'Case Clicker 2' , 'page': 5, 'id': 780 },
   { href: './html/Choppy Orc/index.html', imgSrc: 'choppy orc.jpeg', name: 'Choppy Orc' , 'page': 5, 'id': 790 },

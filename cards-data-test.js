479528767951c68ffa37c30d6e2c9ec954c58a1c
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-12-08 21:47:27 -0700

    Approve game: Update cards-data-test.js

--- cards-data-test.js
+++ cards-data-test.js
@@ -7,7 +7,6 @@ export default [
 { href: './html/Snow Plow/index.html', imgSrc: 'Snow Plow.jpeg', name: 'Snow Plow' , 'page': 5, 'id': 517 },
 { href: './html/Ragdoll Drop/index.html', imgSrc: 'Ragdoll Drop.jpeg', name: 'Ragdoll Drop' , 'page': 5, 'id': 372 },
 { href: './html/Escape Road/index.html', imgSrc: 'Escape Road.jpeg', name: 'Escape Road' , 'page': 5, 'id': 371 },
-{ href: './html/Count Control Legends/index.html', imgSrc: 'Count control Legends.jpeg', name: 'Count Control Legends' , 'page': 5, 'id': 203 },
 { href: './html/Bolts and Nuts/index.html', imgSrc: 'Bolts and Nuts.jpeg', name: 'Bolts and Nuts' , 'page': 5, 'id': 165 },
 { href: './html/Killer/index.html', imgSrc: 'Killer.jpg', name: 'Killer' , 'page': 5, 'id': 901 },
 { href: './html/Hollow knight degraded/index.html', imgSrc: 'hollow knight degraded.jpeg', name: 'Hollow knight degraded' , 'page': 5, 'id': 874 },
